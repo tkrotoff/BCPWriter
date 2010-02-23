fe4642e83e129a7f98a4c09c534285b6f059bedd
Tanguy Krotoff <tkrotoff@gmail.com>
2010-02-23 15:40:13 +0000

    Add BCPChar.cs

---
+++ BCPWriter/BCPChar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BCPWriter
+{
+    class BCPChar
+    {
+        private static readonly string SPACE = " ";
+
+        private string _text;
+        private short _length;
+
+        public BCPChar(string text, short length)
+        {
+            System.Diagnostics.Trace.Assert(text.Length <= length);
+
+            _text = text;
+            _length = length;
+        }
+
+        public void ToBCPFormat(BinaryWriter writer)
+        {
+            //Short is 2 bytes long
+            writer.Write(_length);
+
+            //Append spaces if needed
+            StringBuilder tmp = new StringBuilder(_text);
+            while (tmp.Length < _length)
+            {
+                tmp.Append(SPACE);
+            }
+            ////
+
+            //Text should be in ascii
+            byte[] asciiText = Encoding.Convert(Encoding.Unicode, Encoding.ASCII, Encoding.Unicode.GetBytes(tmp.ToString()));
+            writer.Write(asciiText);
+        }
+    }
+}
--- BCPWriter/Program.cs
+++ BCPWriter/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace BCPWriter
 {
@@ -9,6 +10,13 @@ namespace BCPWriter
     {
         static void Main(string[] args)
         {
+            FileStream stream = new FileStream(@"D:\krotoff\BCPWriter\BCPCharTest", FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            BCPChar bcpChar = new BCPChar("KIKOO", 10);
+            bcpChar.ToBCPFormat(writer);
+
+            writer.Close();
         }
     }
 }
