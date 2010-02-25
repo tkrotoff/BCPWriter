0bdbbe4f81d3fe25e693eb7a499d06c3f3e5e65c
Tanguy Krotoff <tkrotoff@gmail.com>
2010-02-25 11:17:46 +0000

    More documentation on bcp native format

--- BCPWriter/BCPWriter/IBCPSerialization.cs
+++ BCPWriter/BCPWriter/IBCPSerialization.cs
@@ -6,6 +6,36 @@ using System.IO;
 
 namespace BCPWriter
 {
+    /// <summary>
+    /// SQL data types serialization to bcp format.
+    /// </summary>
+    /// <remarks>
+    /// <a href="http://msdn.microsoft.com/en-us/library/ms162802.aspx">bcp Utility</a><br/>
+    /// <br/>
+    /// From SQL Server 2008 Books Online:<br/>
+    /// The bcp utility bulk copies data between an instance of Microsoft SQL Server
+    /// and a data file in a user-specified format.
+    /// The bcp utility can be used to import large numbers of new rows into SQL Server
+    /// tables or to export data out of tables into data files.<br/>
+    /// <br/>
+    /// bcp native (binary) format (-n option) is simple:<br/>
+    /// <br/>
+    /// <code>
+    /// |--------------------|
+    /// | Data Length | Data |
+    /// |--------------------|
+    /// </code>
+    /// <br/>
+    /// Example for a char SQL data type with length 10 and text KIKOO:<br/>
+    /// <code>
+    /// 0A 00 4B 49 4B 4F 4F 20
+    /// 
+    /// |--------------------------------------|
+    /// | Data Length = 10 | Data = KIKOO      |
+    /// | 0A 00            | 4B 49 4B 4F 4F 20 |
+    /// |--------------------------------------|
+    /// </code>
+    /// </remarks>
     interface IBCPSerialization
     {
         void ToBCP(BinaryWriter writer);
